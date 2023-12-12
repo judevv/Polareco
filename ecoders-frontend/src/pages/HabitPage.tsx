1b97ad76643a5f58df9b9dee6f79c94f6bdf1d1e
judevv <judevv604@gmail.com>
2023-12-12 16:06:14 +0900

    feat: habit page css 구조 설정

--- ecoders-frontend/src/pages/HabitPage.tsx
+++ ecoders-frontend/src/pages/HabitPage.tsx
@@ -1,11 +1,69 @@
 import React from "react";
 import styled from "styled-components";
+import { PiStarFourFill } from "react-icons/pi";
+import MyMissions from "../components/feature/user/missions/MyMissions/MyMissions";
+import TodayMissions from "../components/feature/user/missions/TodayMissions/TodayMissions";
+import Button from "../components/atoms/Button";
 
 function HabitPage () {
     return (
-        <div></div>
+        <Container>
+            <ContentsContiner>
+                <Title>
+                    <PiStarFourFill style={{color: '#D3F169'}}/>
+                    <p>Stamps of Week!</p>
+                    <StatusButton>스탬프 현황</StatusButton>
+                </Title>
+                
+            </ContentsContiner>
+            <ContentsContiner>
+                <Title>
+                    <PiStarFourFill style={{color: '#D3F169'}}/>
+                    나만의 미션
+                </Title>
+                <MyMissions />
+            </ContentsContiner>
+            <ContentsContiner>
+                <Title>
+                    <PiStarFourFill style={{color: '#D3F169'}}/>
+                    오늘의 미션
+                </Title>
+                <TodayMissions />
+            </ContentsContiner>
+        </Container>
     )
 
 };
 
 export default HabitPage;
+
+const Container = styled.section`
+    width: auto;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border: 1px solid red; // 임시
+    padding: 30px 120px;
+`;
+
+const ContentsContiner = styled.div`
+    border: 1px solid blue; // 임시
+`;
+
+const Title = styled.div`
+    font-size: 32px;
+    font-weight: 600;
+    line-height: normal;
+    display: flex;
+    flex-direction: row;
+    gap: 14px;
+`;
+
+const StatusButton = styled(Button)`
+    width: auto;
+    height: auto;
+    padding: 0px 20px;
+    color: #fff;
+    background-color: black;
+`;
